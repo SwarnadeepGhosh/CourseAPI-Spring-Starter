0d95a1acf1bcb5410152087be963aa62fd321ce3
Swarnadeep Ghosh <swarna.lyf@gmail.com>
2023-02-09 12:36:04 +0530

    Cors fix

--- src/main/java/com/swarna/courseapi/config/CorsConfig.java
+++ src/main/java/com/swarna/courseapi/config/CorsConfig.java
@@ -3,19 +3,34 @@ package com.swarna.courseapi.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+//@Configuration
+//public class CorsConfig implements WebMvcConfigurer{
+//
+//    // @Value("${frontendURL}")
+//	// private String frontendURL;
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        // WebMvcConfigurer.super.addCorsMappings(registry);
+//        // registry.addMapping("/**").allowedOrigins(frontendURL)
+//        registry.addMapping("/**").allowedOrigins("*")
+//        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+//    }
+//}
+
 @Configuration
-public class CorsConfig implements WebMvcConfigurer{
-
-    // @Value("${frontendURL}")
-	// private String frontendURL;
-
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        // WebMvcConfigurer.super.addCorsMappings(registry);
-        // registry.addMapping("/**").allowedOrigins(frontendURL)
-        registry.addMapping("/**").allowedOrigins("*")
-        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+//            .allowedHeaders("header1", "header2", "header3")
+//            .exposedHeaders("header1", "header2")
+//            .allowCredentials(false).maxAge(3600);
     }
 }
