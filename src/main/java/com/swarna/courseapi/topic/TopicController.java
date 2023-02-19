4df800847a8e68b84d424f400e11ea050ba2e2f4
Swarnadeep Ghosh <swarna.lyf@gmail.com>
2023-02-19 23:21:24 +0530

    adding corsconfig in controllers manually

--- src/main/java/com/swarna/courseapi/topic/TopicController.java
+++ src/main/java/com/swarna/courseapi/topic/TopicController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "https://courseapi.vercel.app")
 public class TopicController {
 	
 	@Autowired
